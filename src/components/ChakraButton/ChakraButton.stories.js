b52c16c5bf6e443fc832e566248f04000c99b365
JunjPan <elisepjj@hotmail.com>
2020-12-27 20:04:26 -0800

    use control and action addons

--- src/components/ChakraButton/ChakraButton.stories.js
+++ src/components/ChakraButton/ChakraButton.stories.js
@@ -3,8 +3,21 @@ import { Button } from '@chakra-ui/react';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  title: 'Chakra/ButtonGreen',
+  title: 'Chakra/Button',
   component: Button,
+  argTypes: {
+    onClick: { action: 'click' },
+  },
+};
+
+//if you want to use control addons insdie the storybook, you have to use args method
+//If you want to use action addons inside the storybook, you have to use args method and have to add  action event inside the argTypes
+const Template = (args) => <Button {...args} />;
+
+export const Danger = Template.bind({});
+Danger.args = {
+  colorScheme: 'red',
+  children: 'Danger',
 };
 
 export const Success = () => (
